a4beba68bb94ff1e754835766a48dcf6f587f84f
MrSun <3225290981@qq.com>
2023-07-16 10:29:59 +0800

    first commit

---
+++ src/main/java/com/MrSun/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.MrSun.git;
+
+/**
+ * @Author MrSun
+ * @Date 2023/7/16 10:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
